1f05e06db711be3b774ec1e385722ce6b9216a37
Gaddi <77565717+RYGJR@users.noreply.github.com>
2021-11-04 23:24:01 +0800

    Testing JPA Repository

--- src/test/java/com/example/demo/student/StudentRepositoryTest.java
+++ src/test/java/com/example/demo/student/StudentRepositoryTest.java
@@ -1,18 +1,24 @@
 package com.example.demo.student;
 
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
+@DataJpaTest
 class StudentRepositoryTest {
 
 
     @Autowired
     private StudentRepository underTest;
 
-
+    @AfterEach
+    void tearDown() {
+        underTest.deleteAll();
+    }
 
     @Test
     void itShouldCheckIfStudentExistsEmail() {
@@ -30,4 +36,15 @@ class StudentRepositoryTest {
         //then
         assertThat(expected).isTrue();
     }
+    @Test
+    void itShouldCheckIfStudentEmailDoesNotExist() {
+        //given
+        String email = "markhamil@gmail.com";
+        //when
+        boolean expected =  underTest.selectExistsEmail(email);
+
+        //then
+        assertThat(expected).isFalse();
+    }
+
 }
