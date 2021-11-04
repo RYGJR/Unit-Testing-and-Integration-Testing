d83baccf2dea8b31a76721858cb3b3b16bfc2a15
Gaddi <77565717+RYGJR@users.noreply.github.com>
2021-11-05 00:07:29 +0800

    Test for when the email is taken

--- src/test/java/com/example/demo/student/StudentServiceTest.java
+++ src/test/java/com/example/demo/student/StudentServiceTest.java
@@ -1,5 +1,6 @@
 package com.example.demo.student;
 
+import com.example.demo.student.exception.BadRequestException;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Disabled;
@@ -12,7 +13,11 @@ import org.mockito.MockitoAnnotations;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.verify;
 
 @ExtendWith(MockitoExtension.class)
@@ -57,6 +62,27 @@ class StudentServiceTest {
         Student capturedStudent = studentArgumentCaptor.getValue();
 
         assertThat(capturedStudent).isEqualTo(student);
+    }
+
+    @Test
+    void whenEmailIsTaken() {
+        //given
+        Student student = new Student(
+                "Hamil",
+                "markhamil@gmail.com",
+                Gender.MALE
+        );
+
+        given(studentRepository.selectExistsEmail(student.getEmail()))
+                .willReturn(true);
+        //when
+        //then
+        assertThatThrownBy(() ->underTest.addStudent(student))
+                .isInstanceOf(BadRequestException.class)
+                .hasMessageContaining("Email " + student.getEmail() + " taken");
+        verify(studentRepository, never()).save(any());
+
+
     }
 
     @Test
